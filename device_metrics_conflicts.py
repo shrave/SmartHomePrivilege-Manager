db86cdcc5dc2e5a2a90d0fc7367547f8489923a6
Shravya Kanchi <shravya.k14@iiits.in>
2020-05-25 20:53:15 +0530

    pushing metrics

--- device_metrics_conflicts.py
+++ device_metrics_conflicts.py
@@ -5,9 +5,6 @@ import pandas as pd
 with open('devices.pkl', 'rb') as input:
     device_instances = pickle.load(input)
 
-for k in device_instances:
-	if k.name == 'Amazon Echo':
-		print(k.user_groups)
 df = pd.read_excel('Device_security.xlsx')
 # print(df.to_dict(orient='dict'))
 df = df.dropna(	)
@@ -37,7 +34,7 @@ for k in device_instances:
 		user_metric_security[k.name][i] = set()
 		for l in k.user_groups.keys():
 			if i in k.user_groups[l]:
-				
+				#here l is a privilege and i is a user number denoting a user group.
 				try:
 					# print((k.name, l, 'pass'))
 					user_metric_privilege[k.name][i].add((l,safety_dict[k.name][l]))
@@ -49,12 +46,42 @@ for k in device_instances:
 		# print('\n')
 	# print('\n')
 # print(user_metric_privilege)
-# for k in user_metric_privilege.keys():
-# 	print(k)
-# 	for j in user_metric_privilege[k].keys():
-# 		print(j)
-# 		print(user_metric_privilege[k][j])
-# 	print('\n')
+tag_dict = {}
+for k in safety_dict.keys():
+	# print(k)
+	tag_dict[k] = []
+	S1_list = []
+	S2_list = []
+	S3_list = []
+	P1_list = []
+	P2_list = []
+	P3_list = []
+	for j in safety_dict[k].keys():
+		# print(j)
+		K = safety_dict[k][j]
+		if '1S' in str(K):
+			S1_list.append(j)
+		if '1P' in str(K):
+			P1_list.append(j)
+		if '2S' in str(K):
+			S2_list.append(j)
+		if '3S' in str(K):
+			S3_list.append(j)
+		if '2P' in str(K):
+			P2_list.append(j)
+		if '3P' in str(K):
+			P3_list.append(j)
+		# print(user_metric_privilege[k][j])
+	# print('\n')
+	tag_dict[k].append([set(S1_list),set(S2_list),set(S3_list)])
+	tag_dict[k].append([set(P1_list),set(P2_list),set(P3_list)])
+	# print(S1_list)
+	# print(S2_list)
+	# print(S3_list)
+	# print(P1_list)
+	# print(P2_list)
+	# print(P3_list)
+# print(tag_dict)
 # comb = combinations(range(1,7), 2) 
 # for k in user_metric_privilege.keys():
 # 	print(k)
@@ -73,3 +100,14 @@ for k in device_instances:
 # 				print('null', end = '<')
 # 		print('\n')	
 # 	print('\n')
+for k in tag_dict.keys():
+	print(k)
+	# print(len(tag_dict[k][0]))
+	# print(len(tag_dict[k][1]))
+	for j in range(len(tag_dict[k][0])):
+		for l in range(len(tag_dict[k][1])):
+			# print(j, end = ' ')
+			# print(l, end = ' ')
+			print(tag_dict[k][0][j].union(tag_dict[k][1][l]), end = '<')
+		print('\n')
+	print('\n')
