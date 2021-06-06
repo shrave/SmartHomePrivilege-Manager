da81e60e307d99d6a91b84a2dc2caf4d0e9e6b58
Shravya Kanchi <shravya.k@research.iiit.ac.in>
2021-06-06 10:20:46 +0530

    resource logs added

--- test_devices_floorplan.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-with open('devices_floorplan.pkl', 'rb') as file:
-	device_list = pickle.load(file)
-
-for device in device_list:
-	# print(dir(device))
-	print(device.label)
-	print(device.privileges)
